c721dd53d24f1569734fc45476952a2c9bd9622f
eggar123 <kea0599@gmail.com>
2023-12-07 19:03:00 +0100

    From room to Mesh Portal
    Changed the scene from a 3d room, to utilizing Mesh Portal from @react-three/drei

--- vite-lockedin/src/App.jsx
+++ vite-lockedin/src/App.jsx
@@ -1,12 +1,28 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import {useProgress,Html,ScrollControls, Scroll} from "@react-three/drei";
+import { Suspense } from "react";
 
 function App() {
+
+  function Loader() {
+    const { active, progress, errors, item, loaded, total } = useProgress()
+    return <Html center className="progress">{Math.round(progress)} % loaded</Html>
+  }
   return (
-    <Canvas shadows camera={{ position: [40, 20, 50]}}>
-      <color attach="background" args={["#000000"]} />
+    
+    <Canvas shadows camera={{ position: [0, 0, 10], fov: 30}}>
+      <Suspense fallback={<Loader/>}>
+     
+      <ScrollControls pages={6} damping={0.1}>
       <Experience />
+      <Scroll html>
+
+      </Scroll>
+      </ScrollControls>
+      </Suspense>
     </Canvas>
+    
   );
 }
 
--- vite-lockedin/src/components/Experience.jsx
+++ vite-lockedin/src/components/Experience.jsx
@@ -1,45 +1,102 @@
-import { OrbitControls } from "@react-three/drei";
+import { Center, Environment, MeshPortalMaterial, OrbitControls, RoundedBox, useTexture } from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { Studio } from "./Studio";
+import * as THREE from "three";
+import { useState } from "react";
+import { Float, Text3D, Shadow } from "@react-three/drei";
 
 export const Experience = () => {
 
-  const adjustIslandForScreenSize = () => {
-    let screenScale, screenPosition;
+  const [active, setActive] = useState(null)
 
-    if (window.innerWidth < 768) {
-      screenScale = [0.9, 0.9, 0.9];
-      screenPosition = [0, -6.5, -43.4];
-    } else {
-      screenScale = [1, 1, 1];
-      screenPosition = [0, -6.5, -43.4];
-    }
-
-    return [screenScale, screenPosition];
-  };
-
-
-
-  const [islandScale, islandPosition] = adjustIslandForScreenSize();
 
 
 
   return (
     <>
+      <ambientLight intensity={0.5} />
+      <Environment preset="sunset" />
       <OrbitControls />
-      <directionalLight position={[1,1,1]} intensity={5}/>
-      <hemisphereLight skyColor="#b1e1ff"/>
-      <Studio
-      position={islandPosition}
-      scale={islandScale}
-      />
-     <group position-y={-1}>
-     <Avatar/>
-     
-     </group>
-     <ambientLight intensity={1}/>
-     
-     
+
+    
+
+        <ProducerStage
+          texture={"textures/Cyberpunk_equirectangular-jpg_Recording_studio_with_ambient_1965269463_9586472.jpg"}
+          name="abedz"
+          active={active}
+          setActive={setActive}
+          >
+
+        </ProducerStage>
+
+        <ProducerStage
+          texture={"textures/Cyberpunk_equirectangular-jpg_Recording_studio_with_ambient_1965269463_9586472.jpg"}
+          name="cronique"
+          position-x={-2.5}
+          rotation-y={Math.PI / 8}
+          active={active}
+          setActive={setActive}
+          >
+
+        </ProducerStage>
+
+        <ProducerStage
+          texture={"textures/Cyberpunk_equirectangular-jpg_Recording_studio_with_ambient_1965269463_9586472.jpg"}
+          name="saint"
+          position-x={2.5}
+          rotation-y={-Math.PI / 8}
+          active={active}
+          setActive={setActive}
+          >
+
+        </ProducerStage>
+
+ 
+
     </>
   );
 };
+
+
+
+
+const ProducerStage = ({ children, texture, name, active, setActive, ...props }) => {
+  const map = useTexture(texture);
+  return (<group {...props}>
+      <Float
+        speed={4} // Animation speed, defaults to 1
+        rotationIntensity={0.1} // XYZ rotation intensity, defaults to 1
+        floatIntensity={0.2} // Up/down float intensity, works like a multiplier with floatingRange,defaults to 1
+        floatingRange={[1, 2]} // Range of y-axis values the object will float within, defaults to [-0.1,0.1]
+      >
+        <mesh />
+    <Center front >
+      <Text3D
+        font={"../../public/fonts/Rubik Bubbles_Regular.json"}
+        bevelEnabled
+        bevelSize={0.03}
+        size={0.1}
+        height={0}
+        position={[0, 2, 0.05]}
+        letterSpacing={0.1}
+      >
+        {name}
+        <meshNormalMaterial />
+      </Text3D>
+    </Center >
+    </Float>
+
+    <RoundedBox args={[2, 3, 0.1]} onDoubleClick={() => setActive(active === name ? null : name)}>
+      
+      <MeshPortalMaterial side={THREE.DoubleSide} blend={active === name ? 1 : 0}>
+        <ambientLight intensity={1} />
+        <Environment preset="sunset" />
+        {children}
+        <mesh>
+          <sphereGeometry args={[2, 64, 64]} />
+          <meshStandardMaterial map={map} side={THREE.BackSide} />
+        </mesh>
+      </MeshPortalMaterial>
+    </RoundedBox>
+  </group>)
+}
